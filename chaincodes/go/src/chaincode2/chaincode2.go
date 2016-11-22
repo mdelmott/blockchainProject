2817e97605aa5538c3c0b861e682b378201390da
mdelmott <delmottemartin@gmail.com>
2016-11-22 15:43:25 +0100

    test

--- chaincodes/go/src/chaincode2/chaincode2.go
+++ chaincodes/go/src/chaincode2/chaincode2.go
@@ -236,7 +236,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(*SimpleChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
